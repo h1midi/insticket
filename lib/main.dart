fe01975eff09c37870c7dcbc8c4bb1d4e1a52472
Hamidi Mohammed <mr.hammiddi@gmail.com>
2022-05-26 15:16:06 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -211,7 +211,7 @@ class _NavBarPageState extends State<NavBarPage> {
                     children: [
                       IconButton(
                           onPressed: () => launchURL(
-                              "https://github.com/hammiddi/projet-PFE/blob/main/README.md"),
+                              "https://github.com/hammiddi/PFE/blob/main/README.md"),
                           icon: Icon(Icons.info_outline)),
                       IconButton(
                           onPressed: () => launchUrl(
@@ -263,7 +263,7 @@ class _NavBarPageState extends State<NavBarPage> {
                           leading: Icon(Icons.info_outline_rounded),
                           onTap: () {
                             launchURL(
-                                "https://github.com/hammiddi/projet-PFE/blob/main/README.md");
+                                "https://github.com/hammiddi/PFE/blob/main/README.md");
                           },
                         ),
                       ),
